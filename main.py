206d93063298a4515838c2ba7b526115a1fafb8e
Krishna Agarwal <kriaga3@gmail.com>
2020-02-29 20:28:43 +0530

    Gmail working Added try catch

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import ElementClickInterceptedException
 
-proxy = "89.33.192.56"
-port = "3128"
+
+
 binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
 # firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
 # firefox_capabilities['marionette'] = True
@@ -20,15 +22,7 @@ binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
 #     "sslProxy": proxy
 # }
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("network.proxy.type", 1)
-profile.set_preference("network.proxy.http", proxy)
-profile.set_preference("network.proxy.http_port", port)
-profile.set_preference("network.proxy.ssl", proxy)
-profile.set_preference("network.proxy.ssl_port", port)
-profile.setPreference("network.proxy.socks", proxy);
-profile.setPreference("network.proxy.socks_port", port);
-profile.update_preferences()
+
 
 
 # driver = webdriver.Firefox(capabilities=firefox_capabilities )
@@ -40,19 +34,56 @@ profile.update_preferences()
 with open('emails.csv', 'r+') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     for row in csv_reader:
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("network.proxy.type", 1)
+        profile.set_preference("network.proxy.http", row[2])
+        profile.set_preference("network.proxy.http_port", row[3])
+        profile.set_preference("network.proxy.ssl", row[2])
+        profile.set_preference("network.proxy.ssl_port", row[3])
+        profile.set_preference("network.proxy.socks", row[2]);
+        profile.set_preference("network.proxy.socks_port", row[3]);
+        profile.update_preferences()
         driver = webdriver.Firefox(firefox_profile=profile, executable_path="D:\\Miscellaneous\\Work\\Shibani\\Freelancer\\google-login\\geckodriver.exe")
-        driver.get("https://mail.google.com/mail/u/1/h/pq3m1hldlteh/")
-        driver.maximize_window()
-        email_phone = driver.find_element_by_xpath("//input[@id='identifierId']")
-        email_phone.send_keys(row[0])
-        driver.find_element_by_id("identifierNext").click()
-        password = WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, "//input[@name='password']"))
-        )
-        password.send_keys(row[1])
-        driver.find_element_by_id("passwordNext").click()
-        driver.find_element_by_xpath("//*[contains(text(), 'Spam')]").click()
-        while driver.find_element_by_xpath("//input[@type='checkbox']"):
-            driver.find_element_by_xpath("//input[@type='checkbox']").click()
-            driver.find_element_by_xpath("//*[contains(text(), 'Not Spam')] | //*[@value='Not Spam']").click()
-        driver.close()
+        try:
+            driver.get("https://mail.google.com/mail/u/1/h/pq3m1hldlteh/")
+            # driver.maximize_window()
+            email_phone = driver.find_element_by_xpath("//input[@id='identifierId']")
+            email_phone.send_keys(row[0])
+            driver.find_element_by_id("identifierNext").click()
+            password = WebDriverWait(driver, 100).until(
+                EC.element_to_be_clickable((By.XPATH, "//input[@name='password']"))
+            )
+            password.send_keys(row[1])
+            next_button = WebDriverWait(driver, 100).until(
+                EC.element_to_be_clickable((By.ID , "passwordNext"))
+            )
+            next_button.click()
+            spammer = WebDriverWait(driver, 100).until(
+                EC.element_to_be_clickable((By.XPATH, "//*[contains(text(), 'Spam')]"))
+            )
+            spammer.click()
+            # driver.find_element_by_xpath("//*[contains(text(), 'Spam')]").click()
+            if driver.find_element_by_xpath("//input[@type='checkbox']"):
+                while driver.find_element_by_xpath("//input[@type='checkbox']"):
+                    driver.find_element_by_xpath("//input[@type='checkbox']").click()
+                    # for probable_unspam_button in driver.find_elements_by_xpath("//input[@type='submit'][@value='Non-spam']"):
+                        # if probable_unspam_button.
+                    # driver.find_element_by_xpath("//input[@value='Non-spam']").click()
+                    driver.find_element_by_xpath("//*[contains(text(), 'Not Spam')] | //input[@type='submit'][@value='Non-spam']").click()
+                    # driver.find_element_by_xpath("//input[@type='submit'][@value='Non-spam']").click()
+            else:
+                print('Nothing to Unpsam now for:', row[0])
+        except ElementClickInterceptedException:
+            print('Failing becuase button is not Interactable for email:', row[0])
+            pass
+        except NoSuchElementException:
+            print('No more mails to Unspam for:', row[0])
+            driver.close()
+            pass
+        except Exception as e:
+            try:
+                driver.close()
+            except:
+                print('Browser Shut')
+            print('Failed for: ', row[0], e)
+            pass
